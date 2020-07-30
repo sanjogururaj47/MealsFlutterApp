9fb55553ce06f3722d0b180dc7d4adea8e341dec
sanjogururaj47 <sanjog.gururaj2@gmail.com>
2020-07-30 14:57:08 +1000

    Added fonts

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:mealsapp/category_item.dart';
 
-class CatogoriesScreen extends StatelessWidget {
+import './dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: <Widget>[],
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lets get this bread'),
+      ),
+      body: GridView(
+        padding: EdgeInsets.all(20),
+        children: DUMMY_CATEGORIES
+            .map((catData) => CategoryItem(catData.title, catData.color))
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 290,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './categories_screen.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -11,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Come get this bread',
       theme: ThemeData.dark(),
-      home: MyHomePage(title: 'GO big or go home'),
+      home: CategoriesScreen(),
     );
   }
 }
